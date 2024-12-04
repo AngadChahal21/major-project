b6bb3a722f8089b574339c68f79525409189d116
Angad Chahal <angadchahal.210@gmail.com>
2024-12-04 11:02:45 -0600

    making the map using tiles

--- sketch.js
+++ sketch.js
@@ -16,9 +16,11 @@ let particles = [];
 let GameState = "startScreen";
 
 // Images 
-let grassImage, waterImage, coinsImage, characterImage;
+let grassImage, waterImage, waterBlockImage, coinsImage, characterImage;
+let grassW, grassH, waterW, waterH;
+let ground, water, coins, character;
 
-//
+let tilemap;
 
 class Particle{
   constructor(x, y, color){
@@ -62,18 +64,54 @@ class Particle{
 
 function preload(){
   menuBackground = loadImage('./pictures/menu-background.jpeg');
-  mainBackground = loadImage('./tileset/1Tiles/ground-tile.png');
-  waterImage = loadImage('./tileset/1Tiles/water.png');
+  mainBackground = loadImage('./tileset/2Background/Background.png');
+
+  waterBlockImage = loadImage('./tileset/1Tiles/singleWater.png');
+  grassImage =  loadImage('./tileset/1Tiles/groundTile.png');
+
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   makeParticles();
-  
+  world.gravity.y = 10;
+  allSprites.pixelPerfect = true;
+
+  grassImage.width = 100;
+  grassImage.height = 100;
+
+  waterBlockImage.width = 100;
+  waterBlockImage.height = 100;
+
+  ground = new Group();
+  ground.layer = 0;
+  ground.collider = "static";
+  ground.img = grassImage;
+  ground.tile = 'g';
+
+  water = new Group();
+  water.layer = 0;
+  water.collider = 'static';
+  water.img = waterBlockImage;
+  water.tile = 'w';
+
+  imageMode(CORNER); 
+
+  tilemap = new Tiles([
+    'ggggggwgg...',
+    '............',
+    '............',
+    '............',
+    'g  ...........',
+    '...........'
+  ],grassImage.width / 2,height - grassImage.height / 2 * 15,grassImage.width, grassImage.height * 1.5);
+ 
 }
 
 function draw() {
-  background();
+  background(mainBackground);
+  fill(0, 120);
+  rect(0, 0, width * 2, height * 2);
   //background(menuBackground);
   //for(let particle of particles){
     //particle.update();
